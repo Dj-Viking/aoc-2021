90a4368a2cb5b759e6180d95f283b098dbea3540
dj-viking <anders.swedishviking@gmail.com>
2021-12-06 10:57:06 -0500

    better solve script

--- solve.sh
+++ solve.sh
@@ -1,5 +1,15 @@
-echo "solving file"
-echo $1
+if [ -z "$1" ]
+  then
+    echo "Please provide a solution file name \nfor example (npm run solve day1)\n .js will be appended"
+    exit
+fi
+
+echo "solving file $1..."
+echo "printing solution..."
+echo ""
 
 cd dist;
 node "$1.js"
+
+echo ""
+echo "done"
