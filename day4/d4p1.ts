b3125bf3a43f06f45b1d959b4ce6228e81145ec7
dj-viking <anders.swedishviking@gmail.com>
2021-12-08 22:12:12 -0500

    created board row matrix

--- day4/d4p1.ts
+++ day4/d4p1.ts
@@ -1,5 +1,7 @@
 import { getInput } from "../utils";
 
+//bingo game win by completing a row, column, or diagonal.
+
 /**
  * object containing all boards with their respective rows
  * stored in a key value pair
@@ -10,7 +12,7 @@ interface BoardMap {
    * board-1, board-2, etc..
    */
   [key: string]: {
-    rows: Array<string>;
+    rows: Array<string | number> | Array<Array<number>>;
   };
 }
 
@@ -21,15 +23,14 @@ interface BoardMap {
       // const splitInput = getInput("../day4/input.txt");
       const sample = getInput("../day4/sample.txt");
       let drawnNums = [] as Array<number>;
+      let newRows = [] as Array<any>;
+      let matchedNums = [] as Array<number>;
+      let newRowsMatrix = [] as Array<Array<number>>;
       let boardMap = {} as BoardMap;
       let boardNum = 0 as number;
       let hitBoundary = 0;
 
-      function getBoardNum(
-        currIdx: number,
-        currentBoardAmount?: number
-      ): number {
-        console.log("current board amount", currentBoardAmount);
+      function getBoardNum(currIdx: number): number {
         if (currIdx >= 1 && currIdx <= 5) {
           return 1;
         } else {
@@ -37,50 +38,108 @@ interface BoardMap {
         }
       }
 
-      //sample array will change in place after shift() which removes drawn numbers
       drawnNums = sample
         .shift()
         ?.split(",")
         .map((str) => parseInt(str)) as Array<number>;
 
-      for (let i = 0; i < sample.length; i++) {
-        console.log(
-          "sample here",
-          sample[i],
-          "curr idx",
-          i,
-          "curr idx mod 6",
-          i % 6
-        );
-        if (sample[i] === "") {
-          hitBoundary++;
-          console.log("hit boundary", hitBoundary);
+      console.log("samples after shifting drawn nums out", sample);
+      // get rid of spaces
+      newRows = sample.filter((str) => str.length > 0);
+      // extract just the numbers separately of 1 or 2 digits
+      matchedNums = newRows
+        .join(" ")
+        .match(/\d{1,2}/g)
+        ?.map((str) => parseInt(str)) as Array<number>;
+
+      //just practicing how to convert the rows in the object to a 5x5 matrix
+      for (let j = 0; j < matchedNums.length; j++) {
+        if (j % 5 === 1) {
+          newRowsMatrix.push([
+            matchedNums[j - 1],
+            matchedNums[j],
+            matchedNums[j + 1],
+            matchedNums[j + 2],
+            matchedNums[j + 3],
+          ]);
         }
-        //not in range if item was empty space at this input[index] so continue the loop
-        // to the next iteration where we will have a new board to push rows onto
+      }
+
+      console.log("new rows", newRows);
+      console.log("matched nums", matchedNums);
+      console.log("newrows matrix", newRowsMatrix);
+
+      for (let i = 0; i < sample.length; i++) {
+        //keep tabs on how many times we hit a boundary
+        // to determine if we increment the board number
+        if (sample[i] === "") hitBoundary++;
         if (sample[i] !== "") {
           boardNum = 0;
           boardNum = getBoardNum(i);
-          let boardAmount = Object.keys(boardMap).length;
-          if (boardAmount > 1 && hitBoundary > 2) {
-            (boardNum as number) = hitBoundary;
-          }
+          if (Object.keys(boardMap).length > 1 && hitBoundary > 2)
+            boardNum = hitBoundary;
           boardMap = {
             ...boardMap,
             [`board-${boardNum}`]: {
               rows:
                 !!boardMap[`board-${boardNum}`] &&
                 !!boardMap[`board-${boardNum}`].rows
-                  ? [...boardMap[`board-${boardNum}`].rows, sample[i]]
+                  ? ([
+                      ...boardMap[`board-${boardNum}`].rows,
+                      sample[i],
+                    ] as Array<string>)
                   : [sample[i]],
             },
           };
         }
       }
-
-      console.log(sample);
-      // console.log("drawn nums", drawnNums);
       console.log("board map", boardMap);
+
+      //convert the board map rows array into an Array<Array<number>>
+      console.log("amount of boards now", Object.keys(boardMap).length);
+      Object.keys(boardMap).forEach((board: string, idx: number) => {
+        if (board === `board-${idx + 1}`) {
+          boardMap = {
+            ...boardMap,
+            [board]: {
+              rows: boardMap[board].rows
+                .join(" ")
+                .match(/\d{1,2}/g)
+                ?.map((str) => parseInt(str)) as Array<number>,
+            },
+          };
+        }
+      });
+
+      console.log("new board map", boardMap);
+
+      for (let b = 0; b < Object.keys(boardMap).length; b++) {
+        boardMap = {
+          ...boardMap,
+          [`board-${b + 1}`]: {
+            rows: boardMap[`board-${b + 1}`].rows
+              .map((_, idx, arr): Array<number> | void => {
+                if (idx % 5 === 1) {
+                  return [
+                    arr[idx - 1] as number,
+                    arr[idx] as number,
+                    arr[idx + 1] as number,
+                    arr[idx + 2] as number,
+                    arr[idx + 3] as number,
+                  ];
+                } else return void 0;
+              })
+              .filter((item) => item !== void 0) as Array<Array<number>>,
+          },
+        };
+      }
+
+      console.log("new board row 1", boardMap["board-1"].rows);
+      console.log("new board row 2", boardMap["board-2"].rows);
+      console.log("new board row 3", boardMap["board-3"].rows);
+
+      // console.log(sample);
+      // console.log("drawn nums", drawnNums);
       resolve();
     } catch (error) {
       reject(error);
--- dist/day4/d4p1.js
+++ dist/day4/d4p1.js
@@ -13,15 +13,17 @@ const utils_1 = require("../utils");
 (function () {
     return __awaiter(this, void 0, void 0, function* () {
         return new Promise((resolve, reject) => {
-            var _a;
+            var _a, _b;
             try {
                 const sample = (0, utils_1.getInput)("../day4/sample.txt");
                 let drawnNums = [];
+                let newRows = [];
+                let matchedNums = [];
+                let newRowsMatrix = [];
                 let boardMap = {};
                 let boardNum = 0;
                 let hitBoundary = 0;
-                function getBoardNum(currIdx, currentBoardAmount) {
-                    console.log("current board amount", currentBoardAmount);
+                function getBoardNum(currIdx) {
                     if (currIdx >= 1 && currIdx <= 5) {
                         return 1;
                     }
@@ -31,29 +33,79 @@ const utils_1 = require("../utils");
                 }
                 drawnNums = (_a = sample
                     .shift()) === null || _a === void 0 ? void 0 : _a.split(",").map((str) => parseInt(str));
-                for (let i = 0; i < sample.length; i++) {
-                    console.log("sample here", sample[i], "curr idx", i, "curr idx mod 6", i % 6);
-                    if (sample[i] === "") {
-                        hitBoundary++;
-                        console.log("hit boundary", hitBoundary);
+                console.log("samples after shifting drawn nums out", sample);
+                newRows = sample.filter((str) => str.length > 0);
+                matchedNums = (_b = newRows
+                    .join(" ")
+                    .match(/\d{1,2}/g)) === null || _b === void 0 ? void 0 : _b.map((str) => parseInt(str));
+                for (let j = 0; j < matchedNums.length; j++) {
+                    if (j % 5 === 1) {
+                        newRowsMatrix.push([
+                            matchedNums[j - 1],
+                            matchedNums[j],
+                            matchedNums[j + 1],
+                            matchedNums[j + 2],
+                            matchedNums[j + 3],
+                        ]);
                     }
+                }
+                console.log("new rows", newRows);
+                console.log("matched nums", matchedNums);
+                console.log("newrows matrix", newRowsMatrix);
+                for (let i = 0; i < sample.length; i++) {
+                    if (sample[i] === "")
+                        hitBoundary++;
                     if (sample[i] !== "") {
                         boardNum = 0;
                         boardNum = getBoardNum(i);
-                        let boardAmount = Object.keys(boardMap).length;
-                        if (boardAmount > 1 && hitBoundary > 2) {
+                        if (Object.keys(boardMap).length > 1 && hitBoundary > 2)
                             boardNum = hitBoundary;
-                        }
                         boardMap = Object.assign(Object.assign({}, boardMap), { [`board-${boardNum}`]: {
                                 rows: !!boardMap[`board-${boardNum}`] &&
                                     !!boardMap[`board-${boardNum}`].rows
-                                    ? [...boardMap[`board-${boardNum}`].rows, sample[i]]
+                                    ? [
+                                        ...boardMap[`board-${boardNum}`].rows,
+                                        sample[i],
+                                    ]
                                     : [sample[i]],
                             } });
                     }
                 }
-                console.log(sample);
                 console.log("board map", boardMap);
+                console.log("amount of boards now", Object.keys(boardMap).length);
+                Object.keys(boardMap).forEach((board, idx) => {
+                    var _a;
+                    if (board === `board-${idx + 1}`) {
+                        boardMap = Object.assign(Object.assign({}, boardMap), { [board]: {
+                                rows: (_a = boardMap[board].rows
+                                    .join(" ")
+                                    .match(/\d{1,2}/g)) === null || _a === void 0 ? void 0 : _a.map((str) => parseInt(str)),
+                            } });
+                    }
+                });
+                console.log("new board map", boardMap);
+                for (let b = 0; b < Object.keys(boardMap).length; b++) {
+                    boardMap = Object.assign(Object.assign({}, boardMap), { [`board-${b + 1}`]: {
+                            rows: boardMap[`board-${b + 1}`].rows
+                                .map((_, idx, arr) => {
+                                if (idx % 5 === 1) {
+                                    return [
+                                        arr[idx - 1],
+                                        arr[idx],
+                                        arr[idx + 1],
+                                        arr[idx + 2],
+                                        arr[idx + 3],
+                                    ];
+                                }
+                                else
+                                    return void 0;
+                            })
+                                .filter((item) => item !== void 0),
+                        } });
+                }
+                console.log("new board row 1", boardMap["board-1"].rows);
+                console.log("new board row 2", boardMap["board-2"].rows);
+                console.log("new board row 3", boardMap["board-3"].rows);
                 resolve();
             }
             catch (error) {
