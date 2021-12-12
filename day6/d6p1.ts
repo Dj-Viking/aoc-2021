dc9afccf8065ea7652a023f31146596bb7810243
dj-viking <anders.swedishviking@gmail.com>
2021-12-12 11:56:11 -0500

    starting to get part 1 down

--- day6/d6p1.ts
+++ day6/d6p1.ts
@@ -1,13 +1,89 @@
 import { getLanternInput } from "../utils";
 
+interface FishTable {
+  [key: string]: number;
+}
+
 (async function (): Promise<void> {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
     try {
       // const lanternInput = getLanternInput("../day6/input.txt");
       // const theInput = lanternInput;
       const lanternInput = getLanternInput("../day6/sample.txt");
       const theInput = lanternInput;
       console.log("theInput", theInput);
+      let fishTable = {} as FishTable;
+      let prevTable = {} as FishTable;
+      let fishDays = [] as Array<number>;
+      fishDays = theInput.map((str) => parseInt(str));
+      console.log("starting fish days'", fishDays);
+
+      function dumpTable(table: FishTable): void {
+        for (let i = 0; i < Object.keys(table).length; i++) {
+          console.log(`${i}: `, table[i]);
+        }
+      }
+      async function copyToPrev(): Promise<void> {
+        return new Promise((resolve) => {
+          prevTable = fishTable;
+          resolve();
+        });
+      }
+
+      //zero the table for all allocated space for each day that a fish lives for
+      function zeroTable(): void {
+        for (let i = 0; i < 9; i++) {
+          fishTable = {
+            ...fishTable,
+            [i]: 0,
+          };
+        }
+      }
+
+      function advanceTable(): void {
+        // make first day state table.
+        for (let d = 0; d < fishDays.length; d++) {
+          for (const key in fishTable) {
+            if (key === fishDays[d].toString()) {
+              fishTable[key]++;
+            }
+          }
+        }
+      }
+
+      // on the next day our first 5 fish will advance a day in their life cycle
+      //
+      async function nextDay(): Promise<void> {
+        // each fish loses a day of their life
+        console.log("fish days before changing", fishDays);
+        for (let f = 0; f < fishDays.length; f++) {
+          fishDays[f]--;
+        }
+        copyToPrev();
+        console.log("previous table");
+        dumpTable(prevTable);
+        //zero the table to then track the new fish days state
+        zeroTable();
+        //increment the days on the table
+        advanceTable();
+        //check if a fish has reached the end of it's life cycle....
+        // make a table of the previous day to compare the arriving current day
+      }
+
+      //begin
+      zeroTable();
+      advanceTable();
+
+      console.log("init table");
+      dumpTable(fishTable);
+
+      nextDay();
+      console.log("next day table");
+      dumpTable(fishTable);
+      nextDay();
+      console.log("next day table");
+      dumpTable(fishTable);
+
       resolve();
     } catch (error) {
       reject(error);
--- dist/day6/d6p1.js
+++ dist/day6/d6p1.js
@@ -12,17 +12,72 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const utils_1 = require("../utils");
 (function () {
     return __awaiter(this, void 0, void 0, function* () {
-        return new Promise((resolve, reject) => {
+        return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
             try {
                 const lanternInput = (0, utils_1.getLanternInput)("../day6/sample.txt");
                 const theInput = lanternInput;
                 console.log("theInput", theInput);
+                let fishTable = {};
+                let prevTable = {};
+                let fishDays = [];
+                fishDays = theInput.map((str) => parseInt(str));
+                console.log("starting fish days'", fishDays);
+                function dumpTable(table) {
+                    for (let i = 0; i < Object.keys(table).length; i++) {
+                        console.log(`${i}: `, table[i]);
+                    }
+                }
+                function copyToPrev() {
+                    return __awaiter(this, void 0, void 0, function* () {
+                        return new Promise((resolve) => {
+                            prevTable = fishTable;
+                            resolve();
+                        });
+                    });
+                }
+                function zeroTable() {
+                    for (let i = 0; i < 9; i++) {
+                        fishTable = Object.assign(Object.assign({}, fishTable), { [i]: 0 });
+                    }
+                }
+                function advanceTable() {
+                    for (let d = 0; d < fishDays.length; d++) {
+                        for (const key in fishTable) {
+                            if (key === fishDays[d].toString()) {
+                                fishTable[key]++;
+                            }
+                        }
+                    }
+                }
+                function nextDay() {
+                    return __awaiter(this, void 0, void 0, function* () {
+                        console.log("fish days before changing", fishDays);
+                        for (let f = 0; f < fishDays.length; f++) {
+                            fishDays[f]--;
+                        }
+                        copyToPrev();
+                        console.log("previous table");
+                        dumpTable(prevTable);
+                        zeroTable();
+                        advanceTable();
+                    });
+                }
+                zeroTable();
+                advanceTable();
+                console.log("init table");
+                dumpTable(fishTable);
+                nextDay();
+                console.log("next day table");
+                dumpTable(fishTable);
+                nextDay();
+                console.log("next day table");
+                dumpTable(fishTable);
                 resolve();
             }
             catch (error) {
                 reject(error);
             }
-        });
+        }));
     });
 })();
 //# sourceMappingURL=d6p1.js.map
