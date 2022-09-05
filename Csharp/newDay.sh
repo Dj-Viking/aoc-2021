d8be5165ea2d6dd8e68afd2bbaf1955e530ba28a
dj-viking <anders.swedishviking@gmail.com>
2022-09-05 17:19:18 -0400

    update new day script

--- Csharp/newDay.sh
+++ Csharp/newDay.sh
@@ -31,9 +31,30 @@ namespace $FOLDER
 {
     class MainClass
     {
+        public string input = "";
+        public string[] _lines = new string[] { "" };
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            new MainClass().Run(args);
+        }   
+        public void Run(string[] args)
+        {
+            this.GetInput(args[0]);
+            this.PartOne();
+            this.PartTwo();
+        }
+        public void GetInput(string fileName)
+        {
+            this.input = File.ReadAllText(fileName);
+            this._lines = input.Split("\n");
+        }
+        public void PartOne()
+        {
+            Console.WriteLine("Part 1: ");
+        }
+        public void PartTwo()
+        {
+            Console.WriteLine("Part 2: ");
         }
     }
 }
