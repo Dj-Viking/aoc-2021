e504237eeb42ce485b9c30fa82af5fe7067b9dac
dj-viking <anders.swedishviking@gmail.com>
2022-09-10 14:21:33 -0400

    solved day 2 part 2 csharp, put sample back into TS day 2

--- Csharp/Day2/Program.cs
+++ Csharp/Day2/Program.cs
@@ -7,16 +7,31 @@ namespace Day2
         public int hPos = 0;
         public int vPos = 0;
         public int tempNum = 0;
-        public string word = "";
+        public int aim = 0;
+        public int forward = 0;
         public static void Main(string[] args)
         {
             new MainClass().Run(args);
         }
         public void Run(string[] args)
         {
+            this.Init();
             this.GetInput(args[0]);
             this.PartOne();
+
+            this.Init();
+            this.GetInput(args[0]);
             this.PartTwo();
+        }
+        public void Init()
+        {
+            this.input = "";
+            this._lines = new string[] { "" };
+            this.hPos = 0;
+            this.vPos = 0;
+            this.tempNum = 0;
+            this.aim = 0;
+            this.forward = 0;
         }
         private static string GetWord(int index, string[] array)
         {
@@ -60,7 +75,36 @@ namespace Day2
         }
         public void PartTwo()
         {
-            Console.WriteLine("Part 2: ");
+            string[] lines = this._lines;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                this.tempNum = GetNumber(i, lines);
+                this.forward = 0;
+                switch (GetWord(i, lines))
+                {
+                    case "forward":
+                        {
+                            this.forward = this.tempNum;
+                            this.hPos += this.tempNum;
+                            if (this.aim == 0)
+                                continue;
+                            if (this.aim > 0)
+                                this.vPos += this.forward * this.aim;
+                        }
+                        break;
+                    case "up":
+                        {
+                            this.aim -= this.tempNum;
+                        }
+                        break;
+                    case "down":
+                        {
+                            this.aim += this.tempNum;
+                        }
+                        break;
+                }
+            }
+            Console.WriteLine("Part 2: {0}", (this.hPos * this.vPos));
         }
     }
 }
