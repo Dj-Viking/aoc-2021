c7716eef89db1052d8e39edf664cde8356a76d96
dj-viking <anders.swedishviking@gmail.com>
2022-11-06 13:58:18 -0500

    start day 5 aoc in c sharp

---
+++ Csharp/Day5/Program.cs
@@ -0,0 +1,40 @@
+namespace Day5
+{
+    class MainClass
+    {
+        public string input = "";
+        public string[] _lines = new string[] { "" };
+        public static void Main(string[] args)
+        {
+            new MainClass().Run(args);
+        }   
+        public void Run(string[] args)
+        {
+            this.Init();
+            this.GetInput(args[0]);
+            this.PartOne();
+
+            this.Init();
+            this.GetInput(args[0]);
+            this.PartTwo();
+        }
+        public void Init()
+        {
+            this.input = "";
+            this._lines = new string[] { "" };
+        }
+        public void GetInput(string fileName)
+        {
+            this.input = File.ReadAllText(fileName);
+            this._lines = input.Split("\n");
+        }
+        public void PartOne()
+        {
+            Console.WriteLine("Part 1: ");
+        }
+        public void PartTwo()
+        {
+            Console.WriteLine("Part 2: ");
+        }
+    }
+}
